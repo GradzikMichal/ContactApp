79585321bb0221dd7eaecfa1bbc0bb1fe533db88
Michal <michalgradzik1000@gmail.com>
2026-02-08 15:56:03 +0100

    Created models for contacts

--- backend/run_backend.sh
+++ backend/run_backend.sh
@@ -4,4 +4,5 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --username ADMIN --noinput --email test@test.pl
 python manage.py collectstatic --no-input --clear
+python manage.py loaddata status_choices
 gunicorn --bind 0.0.0.0:8000 --workers 3 ContactApp.wsgi:application
